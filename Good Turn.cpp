b55d63726858fc858188cd3ae0c6bb90addcacb1
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-03-18 23:27:29 +0530

    Create Good Turn.cpp

---
+++ Good Turn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y,n;
+    cin>>n;
+    while(n--){
+	cin>>x>>y;
+	if (x+y>6)
+	cout<<"YES"<<endl;
+	else{cout<<"NO"<<endl;}}
+	return 0;
+}
