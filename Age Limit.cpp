7a02ce7634801562b2aefa83a8e5d51dfb388b95
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-03-19 22:29:28 +0530

    Create Age Limit.cpp

---
+++ Age Limit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,X,Y,A;
+    cin>>t;
+    while(t--){
+        //int X,Y,A;
+        cin>>X>>Y>>A;
+        if(A>=X && A<Y)
+        cout<<"YES"<<endl;
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+	
+	
+	return 0;
+}
