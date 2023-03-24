29f0bbe1a1e9843f24a12417fe1ba2e7cb2262fb
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-03-24 22:42:39 +0530

    Create Reach Home.cpp

---
+++ Reach Home.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x,y;
+	    if(5*x>=y)
+	    cout<<"yes"<<endl;
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
