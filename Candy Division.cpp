bcd540c70e06cac3bcad9ab80e663d3603dcb671
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-03-24 22:52:35 +0530

    Create Candy Division.cpp

---
+++ Candy Division.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    if(a%3==0)
+	    cout<<"yes"<<endl;
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
