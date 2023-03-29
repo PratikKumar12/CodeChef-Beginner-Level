c4fa927317d169e99e7458e08c47d05f5df295a9
PratikKumar12 <105519168+PratikKumar12@users.noreply.github.com>
2023-03-29 23:41:34 +0530

    Create Burgers.cpp

---
+++ Burgers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b;
+	    if(a>b)
+	    c=b;
+	    else if(a<b)
+	    c=a;
+	    else
+	    (c=b);
+	    cout<<c<<endl;
+	    
+	    
+	    
+	}
+	return 0;
+}
